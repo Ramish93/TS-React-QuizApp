573df5bab1910b7741d34924002190f76dd58053
Ramish Hassan <ramishshah93@gmail.com>
2022-07-01 00:43:38 +0200

    added style file from styled-components, creating global styles next

---
+++ src/App.style.ts
@@ -0,0 +1,8 @@
+import styled, { createGlobalStyle } from "styled-components";
+import background from "./images/background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html{
+    
+}
+`;
